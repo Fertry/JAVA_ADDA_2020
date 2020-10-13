a6b2312007c9ca82245397e7031b5fcb71048878
Alejandro Fernández Trigo <alejandro@fertry.tech>
2020-10-13 17:19:19 +0200

    ¡Ejercicio 1 Completado!
    Función de ejecución del ejercicio 2 comenzada. Arreglar funcionamiento de los cuadrados?

--- Proyecto1AFT/src/main/Proyecto1AFT.java
+++ Proyecto1AFT/src/main/Proyecto1AFT.java
@@ -8,11 +8,11 @@ package main;
 
 // Librerias:
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import us.lsi.flujossecuenciales.StreamsS;
+import us.lsi.math.Math2;
 
 public class Proyecto1AFT {
 	
@@ -20,22 +20,23 @@ public class Proyecto1AFT {
 	public static void main(String[] args) {
 		
 		// Variables:
-		List <Integer> listaEnteros = leeDatosEjercicio1("Tests/PI1Ej1DatosEntrada.txt");
-		//List <Integer> resultadoEjercicio1 = compruebaSiEsPrimo(listaEnteros);
+		List <List <Integer> > listaEnteros = leeDatosEjercicio1("Tests/PI1Ej1DatosEntrada.txt");
+		List <Integer> resultadoEjercicio1 = compruebaSiEsPrimo(listaEnteros);
+		String resultadoEjercicio2 = primosHastaLimite(18);
 		
 		System.out.println("###################################################");
 		System.out.println("################## Test de Ejercicio 1 ############");
 		System.out.println("Datos de entrada --> Lista de enteros: ");
 		System.out.println(listaEnteros);
 		System.out.println("Resultado del ejercicio 1 --> Lista de primos: ");
-		//System.out.println(resultadoEjercicio1);
+		System.out.println(resultadoEjercicio1);
 		System.out.println("###################################################");
 		System.out.println("###################################################");
 		System.out.println("");
 		
 		System.out.println("###################################################");
 		System.out.println("################## Test de Ejercicio 2 ############");
-		//System.out.println("Datos de entrada --> Limite: ");
+		System.out.println(resultadoEjercicio2);
 		System.out.println("###################################################");
 		System.out.println("###################################################");
 		System.out.println("");
@@ -55,53 +56,66 @@ public class Proyecto1AFT {
 	//###################################################################################
 	
 	// Funcion que lee un fichero de entrada; dado un fichero con multiples lineas
-	// y en cada linea numeros enteros separados por comas, devuelve una lista
-	// formada por todos los numeros del fichero:
+	// y en cada linea numeros enteros separados por comas, devuelve una lista de listas
+	// formadas por los numeros del fichero:
 	public static List <List <Integer> > leeDatosEjercicio1 (String fichero) {
 		
 		int i = 0;
-		List <String> lista = StreamsS.file(fichero).collect(Collectors.toList());
-		List <List <Integer> > resultado = new ArrayList <List <Integer> >();
-		while (i < lista.size()) {
-		    
-			String fila = lista.get(i);
-			String[] numeros = fila.split(",");
-			resultado.add(numeros);
-			//resultado.add(Integer.parseInt(numero));			
-			i++;
-					    	
-		}
-			
-		return resultado;
+		int j = 0;
+        List <String> lista = StreamsS.file(fichero).collect(Collectors.toList());
+        List <List <Integer> > resultado = new ArrayList<>();
+        List<Integer> miniLista = new ArrayList<>();
+       
+        while (i < lista.size()) {
+        	
+            String fila = lista.get(i);
+            String[] numeros = fila.split(", ");
+            miniLista = new ArrayList<>();
+            
+            while (j < numeros.length) {
+            	
+            	miniLista.add(Integer.parseInt(numeros[j]));
+                j++;
+                
+            }
+            
+            resultado.add(miniLista);
+            j = 0;
+            i++;
+            
+        }
+        
+        return resultado;
 		  					
 	}
 	
 	// Dada una lista, comprueba que elementos son primos y los
 	// devuelve, todos, en una lista nueva como resultado:
-	public static List <Integer> compruebaSiEsPrimo (List <Integer> lista) {
+	public static List <Integer> compruebaSiEsPrimo (List <List <Integer> > lista) {
 		
+		int i = 0;
+		int j = 0;
 		List <Integer> resultado = new ArrayList <Integer>();
 		
-		for (Integer numero : lista) {
+		while (i < lista.size()) {			
+			while (j < lista.get(i).size()) {
+				
+				if (Math2.esPrimo(lista.get(i).get(j))) {
+					
+					resultado.add(lista.get(i).get(j));
+					
+				}
+				
+				j++;
 			
-			if (esPrimo(numero)) {
-				
-				resultado.add(numero);
-				
 			}
 			
+			j = 0;
+			i++;
+			
 		}
-
+			
 		return resultado;
-		
-	}
-	
-	// Funcion auxiliar que comprueba si un numero dado es
-	// primo o no y devuelve el resultado en forma de una
-	// variable logica:
-	public static boolean esPrimo (Integer numero) {
-		
-		return true;
 		
 	}
 	
@@ -114,9 +128,9 @@ public class Proyecto1AFT {
 	// Funcion que lee un fichero de entrada; dado un fichero con multiples lineas
 	// y en cada linea un concepto "Limite: numero", toma ese numero para pasarlo como
 	// parametro a la funcion del ejercicio 2:
-	public static List <Integer> leeDatosEjercicio2 (String fichero) {
+	public static Integer leeDatosEjercicio2 (String fichero) {
 		
-		List <Integer> resultado = new ArrayList <Integer>();
+		Integer resultado = 0;
 		
 		
 		return resultado;
@@ -124,10 +138,22 @@ public class Proyecto1AFT {
 	}
 	
 	// Dado un numero entero, devuelve una cadena (con saltos de linea) formada por
-	// todos los nº primos desde el 1 hasta el limite (numero introducido como parametro):
+	// todos los nº primos (al cuadrado) desde el 1 hasta el limite (numero introducido como parametro):
 	public static String primosHastaLimite (Integer limite) {
 		
+		int i = 0;
+		int primo = 0;
+		Integer cuadrado = 0;
 		String resultado = "";
+		
+		while (i < limite) {
+			
+			primo = Math2.siguientePrimo(i);
+			cuadrado = primo * primo;
+			resultado = resultado + cuadrado.toString() + "\n";
+			i++;
+			
+		}
 		
 		return resultado;
 		
