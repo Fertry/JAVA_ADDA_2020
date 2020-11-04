cfc43d20d0b9b96966b87297d1ecfc5e0b2c4d7b
Alejandro Fernández Trigo <alejandro@fertry.tech>
2020-11-04 15:30:31 +0100

    Inicio ej 1 funcional

--- PI2FernandezTrigoAlejandro/src/ejercicios/Ejercicio1Funcional.java
+++ PI2FernandezTrigoAlejandro/src/ejercicios/Ejercicio1Funcional.java
@@ -1,5 +1,27 @@
 package ejercicios;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import us.lsi.flujossecuenciales.StreamsS;
+
 public class Ejercicio1Funcional {
 
+	public static List<List<String>> leeDatosEjercicio1Funcional(String fichero) {
+		
+		List<String> lista = StreamsS.file(fichero).collect(Collectors.toList());
+			
+		return Stream.of(lista.spliterator("#"))
+			
+
+	
+		return null;
+		
+	}
+	
+	
 }
