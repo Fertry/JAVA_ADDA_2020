e0e79a1413571fad614ddadf0074d6f4e072f78e
Alejandro Fernández Trigo <alejandro@fertry.tech>
2021-04-05 16:58:03 +0200

    Eliminado de ficheros inservibles

--- PI6FernandezTrigoAlejandro/src/module-info.java
+++ PI6FernandezTrigoAlejandro/src/module-info.java
@@ -1,5 +1,11 @@
 module PI6FernandezTrigoAlejandro {
 	
+	exports ejercicio1;
+	exports ejercicio2;
+	exports ejercicio3;
+	exports ejercicio4;
+	exports ejercicio5;
+	
 	requires partecomun;
 	requires datos_compartidos;
 	requires solve;
