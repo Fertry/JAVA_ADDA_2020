c344c930491c5d756125efa0ee21c19abf422b79
Alejandro Fdez <alejandro@fertry.tech>
2021-04-09 20:50:11 +0200

    Minor changes

--- PI6FernandezTrigoAlejandro/src/ejercicio1/Solucion1.java
+++ PI6FernandezTrigoAlejandro/src/ejercicio1/Solucion1.java
@@ -8,6 +8,13 @@ package ejercicio1;
 
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import us.lsi.flujossecuenciales.StreamsS;
 
 /*
  * Clase solución que pretende parsear la salida del resolvedor Gurobi y la salida
@@ -32,16 +39,64 @@ public class Solucion1 {
 			
 			// Cierra el archivo cuando se acaba de escribir:
 			archivo.close();
-		
+			
+			// Con el fichero creado, se llama a la función que lo parsea:
+			formateo("volcado/salidaPLEj1DatosEntrada" + fichero.replace("ficheros/PI6Ej1DatosEntrada", ""), fichero);
+
 		// Si algo falla, mostramos el contenido de forma directa: 
 		} catch (Exception e) {
 			
 			System.out.println("No se ha podido formatear la salida así que se vuelca el ");
 			System.out.println("resultado directamente por consola.\n");
 			System.out.println(entrada + "\n");
-			//e.printStackTrace();
+			e.printStackTrace();
 			
 		}
+				
+	}
+	
+	public static void formateo(String fichero, String nombre) {
+		
+		// La primera línea representa el valor objetivo.
+		// La segunda línea es descartable.
+		// A partir de la tercera línea comienzan los datos.
+		// El formato de cada línea es: 
+		// x_n_m == 1 dónde n es el alumno y m el grupo.
+		
+		int i = 2;
+		Map<Integer, Integer> reparto = new HashMap<Integer, Integer>();
+		List<String> lista = StreamsS.file(fichero).collect(Collectors.toList());
+				
+		while (i < lista.size()) {
+			
+			String linea = lista.get(i).trim();
+			String[] datos = linea.trim().split("==");
+			
+			// El contenido a la derecha queda descartado dado que siempre es 1
+			// A la izquierda, tras hacer split nos quedan tres valores:
+			// Una x (se descarta), el alumno y el grupo.
+			String[] valores = datos[0].trim().split("_");
+			
+			// Añadir los valores al mapa, esto es: Alumno (como clave) y Grupo (como valor):
+			reparto.put(Integer.parseInt(valores[1].trim()), Integer.parseInt(valores[2].trim()));
+			
+			i++;
+			
+		}
+
+		// Salida final por pantalla:
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println(nombre.replace("ficheros/", "") + ":");	
+		System.out.println("Reparto obtenido:");
+		System.out.println(reparto);
+		
+		for (Integer grupo : reparto.values()) {
+
+			System.out.println("Grupo " + (grupo + 1) + ": ");
+
+		}
+		
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		
 	}
 
